106cbb082f2eb67b96ec3eb95804263668a09185
Manon <manon.bonnet@hetic.net>
2021-10-24 19:34:38 +0200

    update presentation

---
+++ src/php/file.php
@@ -0,0 +1,15 @@
+<!-- <?php 
+
+// $images = scandir("../images/galerie");
+
+// // for($i = 0; $i < strlen($images); $i++) {
+// //   $dom -> $i = $images[$i]; 
+// // }
+
+// $dom -> name = 'john';
+// $dom -> age = 30;
+
+// $jsonpics = json_encode($dom);
+// echo $jsonpics;
+
+?> -->
---
+++ src/script/galery.js
@@ -0,0 +1,13 @@
+// var xmlhttp = new XMLHttpRequest();
+
+
+// xmlhttp.onreadystatechange = function() {
+//   if (this.readyState == 4 && this.status == 200) {
+//     var myObj = JSON.parse(this.responseText);
+//     document.querySelector(".figure-galery").innerHTML = myObj.name;
+//   }
+// };
+
+// xmlhttp.open("GET","../src/php/file.php",true); 
+// xmlhttp.send();
+
