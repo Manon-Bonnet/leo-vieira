15fb7c56e6a75907f582f23f780fb99a2e045b72
Manon <manon.bonnet@hetic.net>
2021-10-24 20:27:53 +0200

    clicable images

--- src/script/galery.js
+++ src/script/galery.js
@@ -11,3 +11,34 @@
 // xmlhttp.open("GET","../src/php/file.php",true); 
 // xmlhttp.send();
 
+// Get elements
+
+const images = document.getElementsByTagName('img');
+const fullWidthBlock = document.getElementsByClassName('fullWidthImage')[0];
+const croce = document.getElementsByClassName('croce')[0];
+
+let tempImage;
+
+// Add Listeners
+for(let i = 0 ; i < images.length ; i++){
+  images[i].addEventListener('click', openImage);
+}
+croce.addEventListener('click', closeImage);
+
+// Functions
+
+function openImage(){
+  tempImage = document.createElement('img');
+  tempImage.src = this.src;
+  tempImage.alt = this.alt;
+
+  console.log(this);
+
+  fullWidthBlock.appendChild(tempImage);
+  fullWidthBlock.classList.remove('hidden');
+}
+
+function closeImage(){
+  fullWidthBlock.classList.add('hidden');
+  fullWidthBlock.removeChild(tempImage);
+}
